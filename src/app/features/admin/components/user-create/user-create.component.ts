3cf03c50f9d478ab21984dd009f5b5b132f10934
TowaSadamori <towa.sadamori@pathoslogos.co.jp>
2025-05-16 07:55:28 +0900

    提出前ルーティング

--- src/app/features/admin/components/user-create/user-create.component.ts
+++ src/app/features/admin/components/user-create/user-create.component.ts
@@ -33,7 +33,7 @@ export class UserCreateComponent implements OnInit {
   private adminUserService = inject(AdminUserService);
   private snackBar = inject(MatSnackBar);
 
-  constructor() { }
+  // constructor() { }
 
   ngOnInit(): void {
     this.userCreateForm = new FormGroup({
