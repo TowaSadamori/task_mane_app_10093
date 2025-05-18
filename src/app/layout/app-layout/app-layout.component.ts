3cea642cf6d44017fd48d9c8c3346c541513d851
TowaSadamori <towa.sadamori@pathoslogos.co.jp>
2025-05-19 00:10:48 +0900

    ヘッダー等見た目いったんOk

--- src/app/layout/app-layout/app-layout.component.ts
+++ src/app/layout/app-layout/app-layout.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from '../../core/components/header/header.component'; // HeaderComponent をインポート
+import { NavigationComponent } from '../../core/components/navigation/navigation.component'; // NavigationComponent をインポート
+import { CommonModule } from '@angular/common'; // CommonModule も念のため追加
 @Component({
   selector: 'app-app-layout',
   standalone: true,
-  imports: [ RouterOutlet],
+  imports: [ 
+    CommonModule, // 追加
+    RouterOutlet,
+    HeaderComponent,    // 追加
+    NavigationComponent // 追加
+  ],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss'
 })
